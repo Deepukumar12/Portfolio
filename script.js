dec3ff93e75e8486cfb6433c92fcdf2edbc692ad
Deepu kumar <deeputhakur0986@gmail.com>
2025-07-26 01:08:26 +0530

    I worked on contact section

--- script.js
+++ script.js
@@ -33,4 +33,13 @@ function theme() {
 
 
 
+// menue toogle
 
+const menuItems = document.querySelectorAll('.nav, .item');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        menuItems.forEach(item => item.classList.remove('active'))
+        item.classList.add('active');
+    });
+});
